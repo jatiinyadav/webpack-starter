2221459e7a1e6c71dd37f84426c6ce6212788eae
Kamal Dev <91242623+devkamal05@users.noreply.github.com>
2022-10-29 01:21:56 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,17 @@ module.exports = {
   mode: "development",
   entry: {
     bundle: path.resolve(__dirname, "src/index.js"),
-    
   },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].js", // using [name] will create files name in entry object. ex: bundle
   },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  }
 };
